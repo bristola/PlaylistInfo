8469b5653b8bc43da7c9724669d634a586bd2ed2
bristola <bristola@allegheny.edu>
2019-11-10 00:01:45 -0500

    Start of styling

--- InfoUI/playlist-info/src/app/app.module.ts
+++ InfoUI/playlist-info/src/app/app.module.ts
@@ -8,19 +8,30 @@ import { PlaylistsComponent } from './components/playlists/playlists.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PlaylistsComponent,
-    SigninComponent
+    SigninComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
